837fc2bdad708a15e99a1adaa52042d0b9a8d857
lugavin <lugavin@outlook.com>
2017-07-05 07:33:47 +0800

    Add Redis

---
+++ ssm-common/src/test/java/com/ssm/common/cache/RedisTest.java
@@ -0,0 +1,36 @@
+package com.ssm.common.cache;
+
+import com.ssm.common.util.Constant;
+import org.junit.Assert;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.concurrent.TimeUnit;
+
+@ActiveProfiles(Constant.ENV_DEV)
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-redis-test.xml"})
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class RedisTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RedisTest.class);
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    public void test() throws Exception {
+        redisTemplate.opsForValue().set("uid", "Gavin", 50, TimeUnit.SECONDS);
+        Assert.assertTrue(redisTemplate.hasKey("uid"));
+    }
+
+}
